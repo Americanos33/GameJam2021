4cbf3d20feefb5656169e4e1d7f0b9917e5b9082
Louis <louis.clerc@etu.univ-grenoble-alpes.fr>
2021-02-08 14:15:16 +0100

    main v1

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+from pygame.locals import *
+
+# Pygame initialization
+pygame.init()
+
+# Keys
+
+
+def drawWindow(surface):
+    pygame.draw.rect(surface, (255,255,255), (100,100,100,100))
+
+def redrawWindow(surface):
+    drawWindow(surface)
+    pygame.display.update()
+
+def main():
+    width = 1024
+    height = 768
+    win = pygame.display.set_mode((width, height))
+
+    flag = True
+    clock = pygame.time.Clock()
+
+    while flag:
+        redrawWindow(win)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                flag = False
+                pygame.display.quit()
+                pygame.quit()
+                sys.quit()
+                break
+
+    pass
+
+
+main()
