d38a3e387e51897af86ed293ce7d6c1d9bb96e4c
Louis <louis.clerc@etu.univ-grenoble-alpes.fr>
2021-02-09 13:24:34 +0100

    changement image perso

--- player.py
+++ player.py
@@ -11,7 +11,7 @@ class Player(pygame.sprite.Sprite) :
         self.velocity = 2
 
         self.image = pygame.image.load("images/personnage1.png").convert_alpha()
-        self.printImage()
+        self.printImage(1)
 
         self.rect = self.image.get_rect()
         self.rect.x = 0
@@ -24,18 +24,22 @@ class Player(pygame.sprite.Sprite) :
         #moves the character to the Right and changes the character image accordingly
         self.rect.x += self.velocity
         self.image = pygame.image.load("images/personnage1.png").convert_alpha()
-        self.printImage()
+        self.printImage(1)
 
     def moveLeft(self):
         
         #moves the character to the Left and changes the character image accordingly
         self.rect.x -= self.velocity
         self.image = pygame.image.load("images/personnage2.png").convert_alpha()
-        self.printImage()
+        self.printImage(2)
 
-    def printImage(self):
+    def printImage(self, nb):
         self.image = pygame.transform.scale(self.image, (64,64))
-        self.image.set_colorkey((240,240,240))
+
+        if nb == 1:
+            self.image.set_colorkey((240,241,241))
+        elif nb == 2:
+            self.image.set_colorkey((240,240,240))
 
     def draw(self, surface) :
         surface.blit(self.image, self.rect)
